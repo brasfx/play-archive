3d4dd08d0bebce776941c6635446af0994d9d22a
Davi Ribeiro <foxpc18@gmail.com>
2025-11-12 16:48:58 -0300

    Rebase project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src'); // Substitua 'src' conforme sua estrutura
+    return config;
+  },
+  // outras opções que você tenha
 };
 
 export default nextConfig;
