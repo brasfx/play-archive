3b1efafdf92ea8256eee9da82f411d4c9f4fea80
Davi Ribeiro <foxpc18@gmail.com>
2025-12-01 12:39:45 -0300

    Create a function  to genarate random icon for app bar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import Link from 'next/link';
 
-import { Gamepad2 } from 'lucide-react';
+import { Gamepad, Gamepad2, GamepadDirectional, Joystick } from 'lucide-react';
 
 import { NavDocuments } from '@/components/nav-documents';
 import { NavMain } from '@/components/nav-main';
@@ -18,13 +18,32 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { useTranslations } from 'next-intl';
 
 import data from '@/data/sidebar-data';
 import { useSession } from 'next-auth/react';
+import { AuroraText } from './ui/aurora-text';
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const { data: session, status } = useSession();
+
+  const icons = [Gamepad, Gamepad2, GamepadDirectional, Joystick];
+
+  function getRandomIcon() {
+    const randomIndex = Math.floor(Math.random() * icons.length);
+    return icons[randomIndex];
+  }
+
+  function RandomIcon() {
+    const [Icon, setIcon] = React.useState<typeof Gamepad | null>(null);
+
+    React.useEffect(() => {
+      setIcon(getRandomIcon());
+    }, []);
+
+    if (!Icon) return null;
+
+    return <Icon className="size-7 animate-bounce text-primary" />;
+  }
 
   return (
     <Sidebar collapsible="offcanvas" {...props}>
@@ -32,9 +51,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             <div className="flex items-center gap-2">
-              <Gamepad2 size={36} />
+              {RandomIcon()}
               <Link href="/">
-                <span className="text-2xl font-semibold ">PlayArchive</span>
+                <span className="text-2xl font-semibold ">
+                  <AuroraText>PlayArchive</AuroraText>
+                </span>
               </Link>
             </div>
           </SidebarMenuItem>
