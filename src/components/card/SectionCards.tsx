a43d97bd20f3d8bef76a9433ed13a865e8664b79
Davi Ribeiro <foxpc18@gmail.com>
2026-01-09 13:11:15 -0300

    Set priority to load image

--- src/components/card/SectionCards.tsx
+++ src/components/card/SectionCards.tsx
@@ -174,6 +174,7 @@ export function SectionCards({
                     width={264}
                     height={100}
                     fetchPriority="high"
+                    priority
                     className="rounded-t-md w-full p-0.5 max-h-[188px] object-cover self-center bg-neutral-500 h-60"
                   />
                 </Lens>
